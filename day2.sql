4821cec888249a2041ce931c06908ac19a8a9814
boopathyarumugam712-hash <boopathyarumugam712@gmail.com>
2025-10-07 22:40:16 +0530

    sql created

---
+++ day2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE customer(
+ customer_id INT PRIMARY KEY AUTO_INCREMENT,
+ customer_name VARCHAR(30),
+ customer_code INT,
+ customer_address VARCHAR(40)
+ );
+ INSERT INTO customer(customer_name,customer_code,customer_address)VALUES("ASDF",1234,"WERR");
+ 
+ ALTER TABLE customer ADD tyui VARCHAR(50);
+ INSERT INTO customer (tyui)VALUES('boopathy');
+ 
+ 
+  SELECT * FROM customer;
+ 
